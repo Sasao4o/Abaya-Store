74804cbbc9039d0b61bde54007c5c907aecc613e
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-12-13 20:40:54 +0200

    tnbgfd

--- Client/src/pages/ViewProduct.jsx
+++ Client/src/pages/ViewProduct.jsx
@@ -26,56 +26,56 @@ export default function ViewProduct() {
     };
     getProduct();
   }, [id]);
-  // return (
-  //   <>
-  //     <div className="view-product">
-  //       <ShoppingCartMessage prodName={productData.name} prodImg={img} />
-  //       <div className="img">
-  //         <img src={img} alt="" />
-  //       </div>
-  //       <div className="product-text">
-  //         <h1 className="product-title">
-  //           {productData.name} {productData.id}
-  //         </h1>
-  //         <h1 className="seller-name">Material: {productData.material}</h1>
-  //         <h2 className="price">{productData.price} L.E</h2>
-  //         <p className="desc">{productData.description}</p>
-  //         {quantity === 0 ? (
-  //           <button
-  //             className="buy-button"
-  //             onClick={() => {
-  //               setIsOpen(true);
-  //               increaseCartQuantity(productData.id);
-  //             }}
-  //           >
-  //             Add to cart
-  //           </button>
-  //         ) : (
-  //           <div className="added-to-cart-section">
-  //             <div className="quantity-number">
-  //               <button
-  //                 onClick={() => {
-  //                   increaseCartQuantity(productData.id);
-  //                 }}
-  //               >
-  //                 +
-  //               </button>
-  //               <p>{quantity}</p>
-  //               <button onClick={() => decreaseCartQuantity(productData.id)}>
-  //                 -
-  //               </button>
-  //             </div>
-  //             <button
-  //               className="remove-button"
-  //               onClick={() => removeFromCart(productData.id)}
-  //             >
-  //               Remove from cart
-  //             </button>
-  //           </div>
-  //         )}
-  //         <button className="buy-button">Buy Now</button>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
+  return (
+    <>
+      <div className="view-product">
+        <ShoppingCartMessage prodName={productData.name} prodImg={img} />
+        <div className="img">
+          <img src={img} alt="" />
+        </div>
+        <div className="product-text">
+          <h1 className="product-title">
+            {productData.name} {productData.id}
+          </h1>
+          <h1 className="seller-name">Material: {productData.material}</h1>
+          <h2 className="price">{productData.price} L.E</h2>
+          <p className="desc">{productData.description}</p>
+          {quantity === 0 ? (
+            <button
+              className="buy-button"
+              onClick={() => {
+                setIsOpen(true);
+                increaseCartQuantity(productData.id);
+              }}
+            >
+              Add to cart
+            </button>
+          ) : (
+            <div className="added-to-cart-section">
+              <div className="quantity-number">
+                <button
+                  onClick={() => {
+                    increaseCartQuantity(productData.id);
+                  }}
+                >
+                  +
+                </button>
+                <p>{quantity}</p>
+                <button onClick={() => decreaseCartQuantity(productData.id)}>
+                  -
+                </button>
+              </div>
+              <button
+                className="remove-button"
+                onClick={() => removeFromCart(productData.id)}
+              >
+                Remove from cart
+              </button>
+            </div>
+          )}
+          <button className="buy-button">Buy Now</button>
+        </div>
+      </div>
+    </>
+  );
 }
