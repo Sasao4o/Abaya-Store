f0a600cde8930e1d6556c2f745babd4129545c81
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-12-13 21:30:20 +0200

    btrdf

--- Client/src/pages/Cart.jsx
+++ Client/src/pages/Cart.jsx
@@ -24,14 +24,14 @@ export default function Cart() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  // useEffect(() => {
-  //   const getProduct = async () => {
-  //     const res = await fetch(`${baseUrl}/api/v1/product`);
-  //     const data = await res.json();
-  //     setProducts(data.data);
-  //   };
-  //   getProduct();
-  // }, []);
+  useEffect(() => {
+    const getProduct = async () => {
+      const res = await fetch(`${baseUrl}/api/v1/product`);
+      const data = await res.json();
+      setProducts(data.data);
+    };
+    getProduct();
+  }, []);
   function calculateTotalCost() {
     return cartItems.reduce((total, cartItem) => {
       const item = products.find((i) => i.id === cartItem.id);
@@ -39,83 +39,83 @@ export default function Cart() {
     }, 0);
   }
   const onSubmit = (data) => console.log(data);
-  return (
-    <div className="cart-view">
-      {cartItemsNumber === 0 ? (
-        <div className="cart-empty">
-          <h1>Shopping cart is empty :c</h1>
-          <Link to="/products">Go Shopping!</Link>
-        </div>
-      ) : (
-        <>
-          <h1>Cart</h1>
-          <div className="cart-view-header">
-            <p>Product</p>
-            <p>Total price</p>
-          </div>
-          <br />
-          <hr />
-          <br />
-          {cartItems.map((item, index) => (
-            <CartItem id={item.id} key={index} />
-          ))}
-          <hr />
-          <div className="total-price">
-            <p className="total-cart-price">Total price</p>
-            <strong>
-              {calculateTotalCost() + shippingCost || calculateTotalCost()} AED
-            </strong>
-          </div>
-          <br />
-          <br />
-          <div className="cart-checkout">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <label>
-                Country / Region: <br />
-              </label>
-              <input type="text" readOnly value="United Arab Emirates" />
-              <label>
-                Address: <br />
-              </label>
-              <input
-                type="text"
-                {...register("address", {
-                  required: "Address is required.",
-                })}
-              />
-              <p className="err">{errors.address?.message}</p>
-              <label>
-                City: <br />
-              </label>
-              <select {...register("city")}>
-                <option value={null}>--Select a city--</option>
-                {cities.map((city, index) => (
-                  <option key={index} value={city}>
-                    {city}
-                  </option>
-                ))}
-              </select>
-              <small>Shipping is free in Abu Dhabi.</small>
-              <p className="err">{errors.city?.message}</p>{" "}
-              <label>
-                ZIP code: <br />
-              </label>
-              <input
-                type="text"
-                {...register("zipCode", {
-                  required: "ZIP code is required.",
-                })}
-              />
-              <label>
-                Discount / Promo code: <br />
-              </label>
-              <input type="text" {...register("discount")} />
-              <p className="err">{errors.zipCode?.message}</p>{" "}
-              <button type="submit">Checkout</button>
-            </form>
-          </div>
-        </>
-      )}
-    </div>
-  );
+  // return (
+  //   <div className="cart-view">
+  //     {cartItemsNumber === 0 ? (
+  //       <div className="cart-empty">
+  //         <h1>Shopping cart is empty :c</h1>
+  //         <Link to="/products">Go Shopping!</Link>
+  //       </div>
+  //     ) : (
+  //       <>
+  //         <h1>Cart</h1>
+  //         <div className="cart-view-header">
+  //           <p>Product</p>
+  //           <p>Total price</p>
+  //         </div>
+  //         <br />
+  //         <hr />
+  //         <br />
+  //         {cartItems.map((item, index) => (
+  //           <CartItem id={item.id} key={index} />
+  //         ))}
+  //         <hr />
+  //         <div className="total-price">
+  //           <p className="total-cart-price">Total price</p>
+  //           <strong>
+  //             {calculateTotalCost() + shippingCost || calculateTotalCost()} AED
+  //           </strong>
+  //         </div>
+  //         <br />
+  //         <br />
+  //         <div className="cart-checkout">
+  //           <form onSubmit={handleSubmit(onSubmit)}>
+  //             <label>
+  //               Country / Region: <br />
+  //             </label>
+  //             <input type="text" readOnly value="United Arab Emirates" />
+  //             <label>
+  //               Address: <br />
+  //             </label>
+  //             <input
+  //               type="text"
+  //               {...register("address", {
+  //                 required: "Address is required.",
+  //               })}
+  //             />
+  //             <p className="err">{errors.address?.message}</p>
+  //             <label>
+  //               City: <br />
+  //             </label>
+  //             <select {...register("city")}>
+  //               <option value={null}>--Select a city--</option>
+  //               {cities.map((city, index) => (
+  //                 <option key={index} value={city}>
+  //                   {city}
+  //                 </option>
+  //               ))}
+  //             </select>
+  //             <small>Shipping is free in Abu Dhabi.</small>
+  //             <p className="err">{errors.city?.message}</p>{" "}
+  //             <label>
+  //               ZIP code: <br />
+  //             </label>
+  //             <input
+  //               type="text"
+  //               {...register("zipCode", {
+  //                 required: "ZIP code is required.",
+  //               })}
+  //             />
+  //             <label>
+  //               Discount / Promo code: <br />
+  //             </label>
+  //             <input type="text" {...register("discount")} />
+  //             <p className="err">{errors.zipCode?.message}</p>{" "}
+  //             <button type="submit">Checkout</button>
+  //           </form>
+  //         </div>
+  //       </>
+  //     )}
+  //   </div>
+  // );
 }
