dbfe586f8072f5007e5bbfb99e3fda43de37c0ee
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-12-13 19:26:02 +0200

    rterv

--- Client/src/components/Product.jsx
+++ Client/src/components/Product.jsx
@@ -1,19 +1,19 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import img from "../assets/images/PB_05195.jpg";
-// import "./styles/product.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../assets/images/PB_05195.jpg";
+import "./styles/product.css";
 
 export default function Product(props) {
-  // return (
-  //   <div className="product">
-  //     <div className="img-container">
-  //       <img src={img} alt="pic" loading="lazy" />
-  //     </div>
-  //     <div className="text">
-  //       <p className="name">{props.name}</p>
-  //       <p className="price">{props.price} DH</p>
-  //       <Link to={`/products/${props.id}`}>View Product</Link>
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className="product">
+      <div className="img-container">
+        <img src={img} alt="pic" loading="lazy" />
+      </div>
+      <div className="text">
+        <p className="name">{props.name}</p>
+        <p className="price">{props.price} DH</p>
+        <Link to={`/products/${props.id}`}>View Product</Link>
+      </div>
+    </div>
+  );
 }
