89049e3c9dc8d2cda4b512489a3dfd3bac9f1b37
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-12-13 19:49:23 +0200

    jymnh

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 import "./styles/navbar.css";
 import { menuItemsData } from "../constants/menuItemsData";
 import { MenuItems } from "./MenuItems";
-import { TbShoppingBag } from "react-icons/tb";
-import { MdMenu } from "react-icons/md";
+// import { TbShoppingBag } from "react-icons/tb";
+// import { MdMenu } from "react-icons/md";
 import { useShoppingCart } from "../contexts/ShoppingCartContext";
-import { IoClose } from "react-icons/io5";
+// import { IoClose } from "react-icons/io5";
 import { MobileMenuItems } from "./MobileMenuItems";
 
 export default function Navbar() {
